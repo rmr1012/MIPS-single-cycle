a5c7df97f994178c39b2237d4ad81cd4d34cbf69
Dennis Ren <rmr1012@users.noreply.github.com>
2018-11-20 17:28:42 -0800

    passed test1

--- RegisterFile.sv
+++ RegisterFile.sv
@@ -9,16 +9,19 @@ module RegisterFile (
 
 logic [32-1:0] file [0:32-1]; //** is the power operator// 8 bit wide, 2^6=64 byte long
 integer i;
-always_comb begin
-	if(rst) begin
-	  for (i=0;i<=31;i=i+1)
+always @ (posedge rst) begin
+	for (i=0;i<=31;i=i+1)
 	    file[i] = 0;
-	end
-	rd1=file[rs];
-	rd2=file[rt];
-	if (RegWrite)begin
+
+end
+always @(wd) begin
+	if (RegWrite) begin
 		file[wn]=wd;
 	end
+end
+always @(rs,rt,wn) begin
+	rd1=file[rs];
+	rd2=file[rt];
 end
 
 //
