32d4245e8dd1136d078639a59c8557b9e1870f27
Dennis Ren <rmr1012@users.noreply.github.com>
2018-11-20 19:25:39 -0800

    passed test2

--- InstructionMemory.sv
+++ InstructionMemory.sv
@@ -1,17 +1,24 @@
 
 module InstructionMemory(input [5:0] PC,
-	output logic [31:0] instruction);
+	input rst,
+	output logic [31:0] instruction,
+	output logic halt);
 
 logic [8-1:0] mem [0:2**6-1]; //** is the power operator// 8 bit wide, 2^6=64 byte long
 
-parameter MEM_INIT = "//Mac/Home/Documents/MIPS-single-cycle/mips1.txt";
+parameter MEM_INIT = "C:\\Users\\juren\\Desktop\\MIPS-single-cycle-master\\mips2.txt";
 
 initial begin
 	$readmemh(MEM_INIT, mem);
 end
 
+
+	
+
 always_comb  begin
+	if(rst) begin halt=0; end
 	instruction = {mem[PC],mem[PC+1],mem[PC+2],mem[PC+3]};
+	halt = ({mem[PC],mem[PC+1],mem[PC+2],mem[PC+3]}==0 || {mem[PC],mem[PC+1],mem[PC+2],mem[PC+3]}=== 32'bX) ? 1:0;
 end
 
 
